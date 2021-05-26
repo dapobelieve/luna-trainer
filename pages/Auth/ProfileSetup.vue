6f4df7f11dba533646542daacd0a715ad5e4cb67
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-05-26 12:33:07 +0100

    pages update

---
+++ pages/Auth/ProfileSetup.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="d-grid gap-4">
+    <div class="col-12">
+      <h3>Setup your profile</h3>
+      <small class="text-muted">Kindly provide all necessary information</small>
+    </div>
+    <div>
+      <form class="d-grid gap-4">
+        <div class="row">
+          <div class="col-md-6">
+            <label for="firstname" class="form-label">First name</label>
+            <input id="firstname" type="text" class="form-control">
+          </div>
+          <div class="col-md-6">
+            <label for="lastname" class="form-label">Last name</label>
+            <input id="lastname" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="">
+          <label for="location" class="form-label">Location</label>
+          <input id="location" type="text" class="form-control">
+        </div>
+        <div class="">
+          <label
+            for="location"
+            class="form-label"
+          >Profile Image</label>
+          <div class="border-dashed d-flex align-items-center rounded p-2">
+            <img class="p-4 rounded-circle border bg-info" src="~/assets/img/user.svg">
+            <div class="ms-4">
+              <small class="d-block"> Drop images here or <u>browse</u> </small>
+              <small class="text-muted">Image format: jpeg, png or gif</small>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="">
+          <label
+            for="experience"
+            class="form-label"
+          >Years of experience</label>
+          <input id="experience" type="text" class="form-control">
+        </div>
+        <div class="">
+          <label
+            for="specialise"
+            class="form-label"
+          >What do you specialise in?</label>
+          <input id="specialise" type="text" class="form-control">
+          <small
+            class="text-muted"
+          >Use comma to separate more than one specialisation.</small>
+        </div>
+        <div class="">
+          <label
+            for="accreditations"
+            class="form-label"
+          >What are your Accreditations?</label>
+          <input id="accreditations" type="text" class="form-control">
+        </div>
+        <div class="">
+          <label
+            for="accreditations"
+            class="form-label"
+          >Do you use positive reinforcement, force free, trust based
+            methods?</label>
+          <div>
+            <input id="yes" type="radio" name="methods" value="yes">
+            <label for="yes">Yes</label> <br>
+            <input id="no" type="radio" name="methods" value="no">
+            <label for="no">No</label>
+          </div>
+        </div>
+        <button
+          type="submit"
+          class="btn primary-color text-white border-0 w-100 mt-2"
+        >
+          Proceed
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProfileSetup',
+  layout: 'authLayout'
+}
+</script>
+<style scoped>
+.border-dashed{
+    border: 1px solid gray;
+    border-style: dashed;
+}
+</style>
---
+++ pages/Auth/SignUp.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="d-grid gap-4">
+    <div class="col-12">
+      <h3>Sign Up</h3>
+    </div>
+    <div class="col-12">
+      <button
+        type="button"
+        class="btn btn-white border w-100 d-flex align-items-center justify-content-center"
+      >
+        <img src="~/assets/img/googleLogoImg.png" alt="google logo">
+        <span class="ms-1">Google</span>
+      </button>
+    </div>
+    <div class="position-relative">
+      <hr>
+      <div
+        class="w-100 position-absolute top-50 translate-middle-y d-flex align-items-center justify-content-center"
+      >
+        <small
+          class="border rounded-circle or-secondary text-muted p-2 body-background"
+        >OR</small>
+      </div>
+    </div>
+    <div>
+      <form>
+        <div class="mb-4">
+          <label for="email" class="form-label fst-normal">Email address</label>
+          <input
+            id="email"
+            type="email"
+            class="form-control"
+            aria-describedby="emailHelp"
+          >
+        </div>
+        <div class="mb-4">
+          <label for="password" class="form-label">Password</label>
+          <input id="password" type="password" class="form-control">
+        </div>
+
+        <div class="mb-4">
+          <label for="password" class="form-label">Confirm Password</label>
+          <input id="password" type="password" class="form-control">
+        </div>
+
+        <button
+          type="submit"
+          class="btn primary-color text-white border-0 w-100 mt-2"
+        >
+          Get started
+        </button>
+      </form>
+      <div class="text-center pt-4">
+        <span
+          class="text-muted"
+        >Already have an account?
+          <NuxtLink
+            :to="{ name: 'Auth-SignIn' }"
+            class="text-dark"
+          >Sign In</NuxtLink></span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SignUp',
+  layout: 'authLayout'
+}
+</script>
+<style scoped></style>
