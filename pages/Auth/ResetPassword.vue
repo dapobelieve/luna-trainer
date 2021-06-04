dd9ad67538a10dd8cf2beffb1dad90146853fa11
manlikeghost <ekpoabasiama@gmail.com>
2021-06-04 15:10:42 +0100

    changed bootstrap classes

--- pages/Auth/ResetPassword.vue
+++ pages/Auth/ResetPassword.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <div class="d-flex justify-content-center pt-3">
+    <div class="tail-flex tail-justify-center tail-pt-3">
       <div class="card w-50 h-50">
-        <div class="p-4  d-grid gap-2">
-          <div class="col-12">
+        <div class="tail-p-4  tail-grid tail-gap-2">
+          <div class="tail-col-12">
             <h4>Create new passowrd</h4>
           </div>
-          <div class="col-12 mb-3">
+          <div class="tail-col-12 tail-mb-3">
             <span
-              class="text-muted"
+              class="tail-text-gray-400"
             >Your new password must be different from previously used
               passwords.</span>
           </div>
           <div>
             <form>
-              <div class="mb-3">
+              <div class="tail-grid tail-mb-3">
                 <label
                   for="exampleInputPassword1"
                   class="form-label"
@@ -25,7 +25,7 @@
                   class="form-control"
                 >
               </div>
-              <div class="mb-3">
+              <div class="tail-grid tail-mb-3">
                 <label
                   for="exampleInputConfirmPassword1"
                   class="form-label"
@@ -37,7 +37,7 @@
                 >
               </div>
 
-              <button type="submit" class="btn btn-primary border-0 w-100">
+              <button type="submit" class="primary-color tail-border-0 tail-w-100 tail-p-1.5 tail">
                 Save
               </button>
             </form>
