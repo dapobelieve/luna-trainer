3a60f9241a081263d707c414e0275619fb6105f2
manlikeghost <ekpoabasiama@gmail.com>
2021-06-24 04:33:15 +0100

    finished create invoice screen

--- components/CreateInvoice.vue
+++ components/CreateInvoice.vue
@@ -35,18 +35,161 @@
                 <label for="item" class="">Add Item</label>
                 <input type="text" class="tail-w-full tail-bg-white tail-p-2.5 tail-block sm:tail-text-sm tail-mt-1 tail-border tail-border-gray-300 tail-rounded-md">
               </div>
-              <div class="tail-border tail-p-4 tail-flex">
-                <div class="tail-flex">
-                  <input type="checkbox" class="tail-p-2">
-                  <span class="tail-text-gray-500">Behaviour Consuming</span>
+              <div class="tail-border tail-p-4 tail-rounded">
+                <div class="tail-flex tail-justify-between tail-p-2">
+                  <div>
+                    <input type="checkbox" class="tail-p-2">
+                    <span class="tail-text-sm">Behaviour Consuming</span>
+                  </div>
+                  <div>
+                    <span>£60</span>
+                  </div>
+                </div>
+                <div class="tail-flex tail-justify-between tail-p-2">
+                  <div>
+                    <input type="checkbox" class="tail-p-2">
+                    <span class="tail-text-sm">Milage</span>
+                  </div>
+                  <div>
+                    <span>£40</span>
+                  </div>
+                </div>
+                <div class="tail-flex tail-p-2">
+                  <div>
+                    <i class="ns-plus tail-text-blue-600" />
+                  </div>
+                  <div>
+                    <span class="tail-text-sm tail-text-blue-600">Add new item</span>
+                  </div>
+                </div>
+              </div>
+              <div class="tail-border tail-rounded tail-mt-4">
+                <div class="tail-p-2">
+                  <div class="tail-flex tail-justify-between tail-py-2">
+                    <div>
+                      <p><bold>Behaviour Consuming</bold></p>
+                      <p class="tail-text-gray-500">
+                        Qty <span>1</span>
+                      </p>
+                    </div>
+                    <div class="tail-p-3">
+                      <span>£60</span>
+                      <span class="tail-border-2 tail-rounded tail-text-center tail-p-1"><i class="ns-menu-dots" /></span>
+                    </div>
+                  </div>
+                  <div class="tail-flex tail-justify-between">
+                    <div>
+                      <p><bold>Milage</bold></p>
+                      <p class="tail-text-gray-500">
+                        Qty <span>1</span>
+                      </p>
+                    </div>
+                    <div class="tail-p-3">
+                      <span>£10</span>
+                      <span class="tail-border-2 tail-rounded tail-text-center tail-p-1"><i class="ns-menu-dots" /></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="tail-pt-5">
+                <hr />
+              </div>
+              <div class="tail-py-3">
+                <input type="checkbox" class="tail-p-2">
+                <span>Value Added Tax (VAT)</span>
+              </div>
+              <div class="">
+                <label for="dueDate" class="">Due date</label>
+                <input type="date" class="tail-w-full tail-bg-white tail-p-2.5 tail-block sm:tail-text-sm tail-mt-1 tail-border tail-border-gray-300 tail-rounded-md">
+              </div>
+              <div class="">
+                <label for="options" class="">Payment Options</label>
+                <input type="text" class="tail-w-full tail-bg-white tail-p-2.5 tail-block sm:tail-text-sm tail-mt-1 tail-border tail-border-gray-300 tail-rounded-md">
+              </div>
+            </div>
+            <button class="primary-color tail-border tail-rounded tail-text-white tail-p-2">
+              Send Invoice
+            </button>
+          </form>
+        </div>
+        <div class="right-card tail-p-4 tail-rounded">
+          <h1>Preview</h1>
+          <span class="tail-text-gray-500">A short description about this section</span>
+          <div class="tail-flex">
+            <div class="">
+              <span>Email</span>
+            </div>
+            <div class="tail-px-5">
+              <span>Invoice PDF</span>
+            </div>
+          </div>
+          <div class="tail-bg-white tail-rounded tail-p-4">
+            <h1>Getwelp Limited</h1>
+            <div class="tail-p-3">
+              <div class="tail-border tail-rounded">
+                <div class="tail-grid tail-grid-cols-2 tail-gap-4 tail-p-5">
+                  <div>
+                    <span>From</span>
+                    <p>Brand Name</p>
+                  </div>
+                  <div>
+                    <span>To</span>
+                    <p>Client's Name</p>
+                  </div>
+                </div>
+                <div class=" tail-p-3">
+                  <div class="tail-bg-gray-300 tail-rounded">
+                    <div class="tail-p-3">
+                      <p>Amount</p>
+                      <p class="tail-text-2xl">
+                        £0.00
+                      </p>
+                      <p class="tail-text-sm">
+                        Due on May 25, 2021
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tail-p-1">
+                  <hr />
+                </div>
+                <div class="tail-p-3">
+                  <div class="tail-flex tail-justify-between">
+                    <div>
+                      <p><bold>Behaviour Consuming</bold></p>
+                      <p class="tail-text-gray-500">
+                        Qty <span>1</span>
+                      </p>
+                    </div>
+                    <div class="tail-p-3">
+                      <span>£60</span>
+                    </div>
+                  </div>
+                  <div class="tail-flex tail-justify-between">
+                    <div>
+                      <p><bold>Milage</bold></p>
+                      <p class="tail-text-gray-500">
+                        Qty <span>1</span>
+                      </p>
+                    </div>
+                    <div class="tail-p-3">
+                      <span>£10</span>
+                    </div>
+                  </div>
+                  <div class="tail-p-1">
+                    <hr />
+                  </div>
+                  <div class="tail-flex tail-justify-between">
+                    <div>
+                      <p><bold>Amount Due</bold></p>
+                    </div>
+                    <div class="tail-pr-2">
+                      <span>£70</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </form>
-        </div>
-        <div class="right-card">
-          <div class="tail-border tail-rounded">
-            <span>Preview</span>
           </div>
         </div>
       </div>
@@ -67,13 +210,14 @@ export default {
 <style scoped>
 .card {
   background-color: #fff;
-  height: 100vh;
+  padding: 10px;
 }
 .left-card {
   width: 26.25rem;
 }
 .right-card {
   width: 26.25rem;
+  height: 51.25rem;
   background-color: #f0f5fa;
 }
 </style>
