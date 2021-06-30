419d115f0f424e6f92c7d3205e93b1d04444b3f1
manlikeghost <ekpoabasiama@gmail.com>
2021-06-30 09:57:05 +0100

    client list on dashboard ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,9 +61,9 @@ export default {
     // baseURL: 'https://api.getwelp.co.uk/accounts/v0'
   },
 
-  router: {
-    middleware: ['auth']
-  },
+  // router: {
+  //   middleware: ['auth']
+  // },
 
   auth: {
     redirect: {
