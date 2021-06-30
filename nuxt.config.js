dc6c6ad965e14cd2f28c7c9032be855e08fda362
manlikeghost <ekpoabasiama@gmail.com>
2021-06-30 15:18:29 +0100

    sign up screen authenticated

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~plugins/v-calendar.js', ssr: false },
-    { src: '~/plugins/vuelidate.js', ssr: false },
+    { src: '~/plugins/vuelidate.js', ssr: true },
     { src: '~plugins/persistedState.client.js' }
   ],
 
