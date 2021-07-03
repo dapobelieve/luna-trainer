1a7acec82d29d75a481a4505b64c69cb7e6fdeb2
uchechukwu nwulu <uchedotphp@gmail.com>
2021-07-03 05:49:46 +0100

    updated styling to dropdown notifications

--- components/NotificationDropdown.vue
+++ components/NotificationDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tail-absolute tail-right-0 tail-border tail-overflow-hidden tail-rounded-xl tail-bg-white tail-border-gray-100 tail-shadow-2xl tail-top-full tail-mr-3 tail-max-w-xs">
+  <div class="tail-absolute tail-right-0 tail-border tail-overflow-hidden tail-rounded-xl tail-bg-white tail-border-gray-200 tail-shadow-2xl tail-top-full tail-mr-3 tail-max-w-xs">
     <div class="tail-block tail-rounded tail-bg-white">
       <div class="tail-border-b">
         <div class="tail-px-4 tail-pt-5 tail-pb-4 tail-bg-white">
@@ -8,8 +8,8 @@
           </span>
         </div>
       </div>
-      <div class="tail-grid tail-grid-cols-1 tail-gap-3 tail-p-4" style="background: #F0F5FA">
-        <div class="tail-flex tail-items-center">
+      <div class="tail-grid tail-grid-cols-1" style="background: #F0F5FA">
+        <div class="tail-flex tail-items-center hover:tail-bg-gray-200 tail-py-3 tail-px-4 tail-cursor-pointer">
           <img
             class="tail-rounded-full tail-h-11"
             src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
@@ -24,7 +24,7 @@
             </p>
           </div>
         </div>
-        <div class="tail-flex tail-py-1">
+        <div class="tail-flex tail-py-4 tail-px-4">
           <div class="tail-bg-gray-200 tail-w-12 tail-h-12 tail-rounded-full tail-flex tail-justify-center tail-items-center">
             <i
               class="ns-bell-ring tail-text-gray-600"
