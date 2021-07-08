67beb2e4a9a97abb114e14e80c60fc51add6dc4c
manlikeghost <ekpoabasiama@gmail.com>
2021-07-08 23:11:18 +0100

    button created

---
+++ components/util/AsyncButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
