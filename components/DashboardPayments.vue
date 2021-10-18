24141263b6dada7d61ee204b443805ffe1ab195b
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-10-18 09:56:38 +0100

    feat[date-time-plugin]: formating date with how long ago

--- components/DashboardPayments.vue
+++ components/DashboardPayments.vue
@@ -8,10 +8,12 @@
         class="ns-receipt tail-bg-indigo-50 tail-p-1 tail-rounded-full tail-text-indigo-500 tail-text-2xl tail-h-12 tail-w-12 tail-flex tail-items-center tail-justify-center tail-flex-shrink-0"
       ></i>
     </template>
-    <template v-slot:title>invoices</template>
+    <template v-slot:title>
+      invoices
+    </template>
     <template v-slot:notifications>
       {{ paidInvoices.length }} new notification{{
-      paidInvoices.length > 1 ? "s" : ""
+        paidInvoices.length > 1 ? "s" : ""
       }}
     </template>
     <template v-slot:content>
@@ -32,14 +34,18 @@
                 <span class="tail-font-medium">{{ invoice.customerId.firstName }}
                   {{ invoice.customerId.lastName }}</span> has paid you.
               </template>
-              <template v-slot:date>{{ invoice.dueDate | date }}</template>
+              <template v-slot:date>
+                {{ invoice.dueDate | howLongAgo }}
+              </template>
             </containers-summary-information-with-avatar>
           </li>
         </ul>
         <div
           v-else
           class="tail-text-center tail-pt-8 tail-pb-12 tail-px-4 tail-text-gray-500 tail-text-sm"
-        >Newly paid invoices will be displayed here.</div>
+        >
+          Newly paid invoices will be displayed here.
+        </div>
       </template>
     </template>
   </containers-summary-card-with-notifications>
