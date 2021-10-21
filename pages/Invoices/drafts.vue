19b3eff33a428c076999423c54b036c73469fc59
Dapo Michaels <dapomichaels@gmail.com>
2021-10-21 20:13:47 +0100

    sent invoice

--- pages/Invoices/drafts.vue
+++ pages/Invoices/drafts.vue
@@ -1,31 +1,12 @@
 <template>
-  <div class="tail-flex tail-px-8 tail-mt-8">
-    <div class="tail-max-w-full tail-flex tail-border tail-mt-8 tail-rounded-lgs tail-border-gray-200">
-      <div class="tail-bg-white tail-w-full">
-<!--          <ClientAvatar :clientInfo="data.customerId" />-->
-        <div>
-          <img class="tail-h-5 tail-w-5 tail-bg-gray-400">
-        </div>
-        <div class="tail-ml-4">
-          <h6 class="tail-text-base tail-text-gray-700 tail-text-capitalize">Dapo Believe</h6>
-          <span class="tail-text-sm tail-text-gray-500">believe@gmail.com</span>
-        </div>
-      </div>
-    </div>
+  <div>
+
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      invoices: null
-    }
-  },
-  async asyncData(ctx) {
-    let res = await ctx.store.dispatch('invoice/getInvoices', {status: 'draft'})
-    return {invoices: res}
-  }
+  
 }
 </script>
 
