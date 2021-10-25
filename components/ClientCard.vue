1056f41cadcc5d586cf2bc5d1679524b18d28f45
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-10-25 06:37:40 +0100

    fix[creating-invoice]: added route param with client info.

--- components/ClientCard.vue
+++ components/ClientCard.vue
@@ -34,7 +34,7 @@
         >
           <div class="tail-py-2" role="none">
             <nuxt-link
-              :to="{ name: 'CreateInvoice' }"
+              :to="{ name: 'NewInvoice', params: { pushedClient: client } }"
               class="tail-text-gray-700 tail-block tail-px-4 tail-py-2 tail-text-sm hover:tail-bg-gray-100"
             >
               Create Invoice
