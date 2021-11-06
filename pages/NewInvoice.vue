332245d2a688035709d440ea072bb9ca69818a78
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-11-06 12:11:24 +0100

    fix: saving invoice before sending

--- pages/NewInvoice.vue
+++ pages/NewInvoice.vue
@@ -58,8 +58,8 @@
           </gw-customer-selector>
         </div>
         <div class="flex items-center justify-between">
-          <p class="capitalize text-xl font-normal">
-            invoice service &amp; items
+          <p class="text-xl font-normal">
+            Invoice items
           </p>
           <button type="button" @click="$modal.show('add-service-modal')">
             <img
@@ -72,47 +72,50 @@
         </div>
         <div class="space-y-6">
           <div class="flex flex-col space-y-2">
-            <label class="input-text-label">
-              Choose
-            </label>
-            <gw-customer-selector
-              multiple
-              :clients="$auth.user.services"
-              @change="invoiceDetails.services = $event"
-            >
-              <template v-slot:dropdownOption="{ optionObject }">
-                <div class="flex justify-between min-w-full items-center">
-                  <div class="flex content-center py-1">
-                    <div class="flex flex-col ml-1 text-gray-700">
-                      <p class="capitalize mb-2">
-                        {{ optionObject.description }}
-                      </p>
-                      <small class="text-gray-700 text-sm">
-                        {{ optionObject.pricing.amount | amount }}</small>
+            <div>
+              <label class="input-text-label">
+                Choose
+              </label>
+              <gw-customer-selector
+                multiple
+                :disabled="!invoiceDetails.client"
+                :clients="$auth.user.services"
+                @change="invoiceDetails.services = $event"
+              >
+                <template v-slot:dropdownOption="{ optionObject }">
+                  <div class="flex justify-between min-w-full items-center">
+                    <div class="flex content-center py-1">
+                      <div class="flex flex-col ml-1 text-gray-700">
+                        <p class="capitalize mb-2">
+                          {{ optionObject.description }}
+                        </p>
+                        <small class="text-gray-700 text-sm">
+                          {{ optionObject.pricing.amount | amount }}</small>
+                      </div>
+                    </div>
+                    <div class="check">
+                      <i class="ns-check text-blue-500 text-lg"></i>
                     </div>
                   </div>
-                  <div class="check">
-                    <i class="ns-check text-blue-500 text-lg"></i>
-                  </div>
-                </div>
-              </template>
-              <template v-slot:footer>
-                <button
-                  type="button"
-                  class="py-2 outline-none"
-                  @click="$modal.show('add-service-modal')"
-                >
-                  <div class="flex px-2 ml-1 items-center justify-center">
-                    <i
-                      class="ns-plus text-base rounded-full text-blue-500 p-1"
-                    />
-                    <span
-                      class="text-primary-color text-base pl-2"
-                    >Add New Item</span>
-                  </div>
-                </button>
-              </template>
-            </gw-customer-selector>
+                </template>
+                <template v-slot:footer>
+                  <button
+                    type="button"
+                    class="py-2 outline-none"
+                    @click="$modal.show('add-service-modal')"
+                  >
+                    <div class="flex px-2 ml-1 items-center justify-center">
+                      <i
+                        class="ns-plus text-base rounded-full text-blue-500 p-1"
+                      />
+                      <span
+                        class="text-primary-color text-base pl-2"
+                      >Add New Item</span>
+                    </div>
+                  </button>
+                </template>
+              </gw-customer-selector>
+            </div>
             <div
               v-if="invoiceDetails.services.length"
               class="rounded-xl border bg-gray-50 py-4 px-3 space-y-3"
@@ -130,32 +133,8 @@
                     {{ service.subtitle }}
                   </p>
                 </div>
-                <div class="flex items-center space-x-2">
-                  <span> {{ service.pricing.amount | amount }} </span>
-                  <div class="relative">
-                    <button type="button" @click="toggleShowDropdown">
-                      <img src="~/assets/img/svgs/ellipsis.svg" alt="" />
-                    </button>
-                    <!-- dropdown menu -->
-                    <div
-                      v-show="showDropDown"
-                      class="origin-top-right absolute right-0 mt-2 w-44 rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-40"
-                    >
-                      <div class="py-2" role="none">
-                        <button
-                          type="button"
-                          class="dropdown-button"
-                          @click="editServiceItem(service)"
-                        >
-                          Edit
-                        </button>
-                        <button type="button" class="dropdown-button">
-                          Delete
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <InvoicesServiceDisplay :service="service" @edit-service="editServiceItem($event)">
+                </InvoicesServiceDisplay>
               </div>
             </div>
           </div>
@@ -167,7 +146,7 @@
             <label for="dueDate" class="input-text-label">Due date</label>
             <date-picker
               v-model="invoiceDetails.dueDate"
-              style="width: 100% !important"
+              style="width: 100% !important;"
               class="w-full"
               :disabled-date="date => date < new Date()"
               value-type="format"
@@ -316,34 +295,44 @@ export default {
     async send () {
       try {
         this.isLoading = true
-        const sending = await this.sendInvoice({ id: this.invoiceId })
-        if (sending.status === 'success') {
-          this.$toast.success('Invoice sending successful', {
-            position: 'top-right'
-          })
-          this.$router.push({ name: 'Invoices-sent' })
+        if (this.invoiceId) {
+          const sending = await this.sendInvoice({ id: this.invoiceId })
+          if (sending.status === 'success') {
+            this.$toast.success('Invoice sending successful', {
+              position: 'top-right'
+            })
+          }
+        } else {
+          const createInvoice = await this.createNewInvoice(
+            this.invoiceToBeSent
+          )
+          if (createInvoice.status === 'success') {
+            const invoiceId = createInvoice.data._id
+            const sending = await this.sendInvoice({ id: invoiceId })
+            if (sending.status === 'success') {
+              this.$toast.success('Invoice sending successful', {
+                position: 'top-right'
+              })
+            }
+          }
         }
+        this.$router.push({ name: 'Invoices-sent' })
       } catch (error) {
         this.isLoading = false
-        const errorResponse = this.$errorHandler.setAndParse(error)
-        this.$toast.error(
-            `Something went wrong: ${errorResponse.message}`,
+        if (error.response) {
+          this.$toast.error(
+            `Something went wrong: ${error.response.data.message}`,
             { position: 'bottom-right' }
-        )
-        // if (error.response) {
-        //   this.$toast.error(
-        //     `Something went wrong: ${error.response.data.message}`,
-        //     { position: 'bottom-right' }
-        //   )
-        // } else if (error.request) {
-        //   this.$toast.error('Something went wrong. Try again', {
-        //     position: 'bottom-right'
-        //   })
-        // } else {
-        //   this.$toast.error(`Something went wrong: ${error.message}`, {
-        //     position: 'bottom-right'
-        //   })
-        // }
+          )
+        } else if (error.request) {
+          this.$toast.error('Something went wrong. Try again', {
+            position: 'bottom-right'
+          })
+        } else {
+          this.$toast.error(`Something went wrong: ${error.message}`, {
+            position: 'bottom-right'
+          })
+        }
       }
     },
     updateInvoice () {
