27df910d7cb6577b30b44bc9274210c7b7745786
Dapo Michaels <dapomichaels@gmail.com>
2022-01-28 12:34:58 +0100

    updates

--- pages/reports/index.vue
+++ pages/reports/index.vue
@@ -5,22 +5,28 @@
         <span class="font-normal">Reporting</span>
       </template>
     </PageHeader>
-    <div class="w-full">
-      
+    <div class="bg-white py-2 flex items-center">
+      <div class="p-3">Financial</div>
+      <div class="p-3">Client</div>
     </div>
-    <div class="grid gap-4">
-      <div class="bg-white rounded-md p-3">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
-      </div>
-      <div class="grid gap-4 lg:grid-cols-2 rounded-md ">
-        <div class="bg-white rounded-md p-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
+
+    <div class="w-full p-4 bg-red-100 flex justify-center">
+      <div class="max-w-xl 2xl:max-w-7xl md:max-w-4xl lg:max-w-full bg-black">
+        <div class="grid gap-4">
+          <div class="grid gap-4 lg:grid-cols-2 rounded-md ">
+            <div class="bg-white rounded-md p-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
+            </div>
+            <div class="bg-white rounded-md p-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
+            </div>
+          </div>
+          <div class="bg-white rounded-md p-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
+          </div>
         </div>
-        <div class="bg-white rounded-md p-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequatur corporis debitis delectus dolorem enim facere illo laborum nemo nulla perspiciatis, quidem repellendus sapiente soluta ullam unde voluptate voluptatum.
-        </div>
       </div>
-    </div>
+    </div>    
   </div>
 </template>
 
