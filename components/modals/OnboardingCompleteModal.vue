e9b51f81eb266b017a70206f2251a900223a7a71
Maison Armani <masonarmani38@gmail.com>
2022-03-10 00:18:02 +0100

    quick mini fixes

--- components/modals/OnboardingCompleteModal.vue
+++ components/modals/OnboardingCompleteModal.vue
@@ -2,11 +2,6 @@
   <modal name="done" height="auto" :adaptive="true" :max-width="500" :click-to-close="false">
     <div class="m-8">
       <div class="flex justify-start flex-col mt-1 mb-2">
-        <img
-          src="~/assets/img/svgs/check-icon.svg"
-          alt="checked"
-          class="h-5 w-5 mb-2"
-        />
         <h3 class="my-2 gray-700 text-xl lg:text-2xl">
           All done!
         </h3>
