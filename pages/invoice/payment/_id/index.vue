0c90f9fb5a522ac3531caf8e39170f7d8b35a99e
Victor Umesiobi <victor.umesiobi@inheaden.io>
2022-03-16 17:04:31 -0700

    feat: invoice payment ui

---
+++ pages/invoice/payment/_id/index.vue
@@ -0,0 +1,34 @@
+<template></template>
+<script>
+export default {
+  name: 'InvoicePayment',
+  layout: 'InvoicePayment',
+  middleware: ['validToken'],
+  auth: false,
+  data() {
+    return {}
+  },
+  head() {
+    return {
+      title: 'InvoicePayment',
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.base-button {
+  &:hover {
+    background: #46a6c8;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+.error {
+  @apply border-red-500;
+}
+.required:after {
+  content: ' *';
+  @apply text-red-500 text-sm;
+}
+</style>
