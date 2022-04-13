b56f26f0d6573daeb33a14361039c25a1cfd6f76
Victor Umesiobi <victor.umesiobi@demzanalytics.com>
2022-04-13 13:10:20 +0100

    fix: moved to nuxt.config

--- middleware/dashboard.js
+++ middleware/dashboard.js
@@ -1,8 +1,6 @@
-import hotJar from "../utils/hotjar-track";
 export default function ({ redirect, route, $auth }) {
   // If the user is not authenticated and path is root dir
   // console.log($auth.state.user)
-  const track = hotJar;
   if (route.path === "/") {
     return redirect("/dashboard");
   }
