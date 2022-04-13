b445afa25667a2610b401d7b06cf80ca8ce02377
Victor Umesiobi <victor.umesiobi@demzanalytics.com>
2022-04-13 13:03:34 +0100

    fix: added hotjar

--- middleware/dashboard.js
+++ middleware/dashboard.js
@@ -1,7 +1,9 @@
+import hotJar from "../utils/hotjar-track";
 export default function ({ redirect, route, $auth }) {
   // If the user is not authenticated and path is root dir
   // console.log($auth.state.user)
-  if (route.path === '/') {
-    return redirect('/dashboard')
+  const track = hotJar;
+  if (route.path === "/") {
+    return redirect("/dashboard");
   }
 }
