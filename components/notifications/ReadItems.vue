a7796fd9ed56932bbdff3484cb645b65221606ba
Maison Armani <masonarmani38@gmail.com>
2022-04-20 19:39:41 +0100

    update notifications count

--- components/notifications/ReadItems.vue
+++ components/notifications/ReadItems.vue
@@ -6,29 +6,61 @@
         :key="notification._id"
         :notification="notification"
       />
+      <button v-if="this.readNotifications.length === this.page * this.limit" @click.prevent="loadMore" class="btn block w-full py-2 font-bold">
+        <single-loader  v-if="loadingMore"> </single-loader>
+        <span v-else>Load More</span>
+      </button>
     </template>
     <NoNotifications v-else />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import NoNotifications from '~/components/notifications/NoNotifications.vue'
-import NewNotification from '~/components/notifications/NewNotification.vue'
+import { mapGetters, mapActions } from 'vuex'
+import SingleLoader from '../util/SingleLoader.vue'
+import NoNotifications from '~/components/notifications/NoNotifications'
+import NewNotification from '~/components/notifications/NewNotification'
 
 export default {
   name: 'ReadItems',
   components: {
+    NewNotification,
     NoNotifications,
-    NewNotification
+    SingleLoader
+  },
+  data () {
+    return {
+      page: 1,
+      limit: 10,
+      loadingMore: false
+    }
+  },
+  methods: {
+    ...mapActions({
+      fetchNotifications: 'notifications/fetchNotifications'
+    }),
+    async loadMore () {
+      this.page++
+      this.loadingMore = true
+      await this.fetchNotifications({
+        page: this.page,
+        limit: this.limit,
+        status: 'READ'
+      })
+      this.loadingMore = false
+    }
+  },
+  async fetch () {
+    await this.fetchNotifications({
+      page: this.page,
+      status: 'READ',
+      limit: this.limit
+    })
   },
   computed: {
     ...mapGetters({
-      notifications: 'notifications/getAllNotifications'
-    }),
-    readNotifications () {
-      return this.notifications.filter(n => n.status === 'READ' && n.type !== 'LOGIN_WITH_QR')
-    }
+      readNotifications: 'notifications/getReadNotifications'
+    })
   }
 }
 </script>
