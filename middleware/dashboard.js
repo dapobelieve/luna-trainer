15797fb1a948c22ca64e3049df69e9e1dd7211b9
Dapo Michaels <dapomichaels@gmail.com>
2022-04-20 09:58:33 +0100

    add gzip and brotli compressions

--- middleware/dashboard.js
+++ middleware/dashboard.js
@@ -1,6 +1,7 @@
 export default function ({ redirect, route, $auth }) {
   // If the user is not authenticated and path is root dir
   // console.log($auth.state.user)
+  console.log('middleware here')
   if (route.path === "/") {
     return redirect("/dashboard");
   }
--- store/index.js
+++
@@ -1,5 +0,0 @@
-export const actions = {
-  nuxtServerInit ({ commit }, ) {
-    console.log('nuxtServerInit')
-  },
-}
