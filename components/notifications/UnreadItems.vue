ffdf18ff1fa3d775f8717b0182dae9dc624b0d8a
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2022-04-25 13:07:00 +0100

    [feat] Add correct values for mobile nav messages and notifications count and fix updating country

--- components/notifications/UnreadItems.vue
+++ components/notifications/UnreadItems.vue
@@ -7,10 +7,12 @@
         :notification="notification"
       />
       <button
-        v-if="this.unreadNotifications.length >= this.page * this.limit || loadingMore"
+        v-if="unreadNotifications.length >= page * limit || loadingMore"
+        class="btn w-full py-2 font-bold text-center flex items-center"
         @click.prevent="loadMore"
-        class="btn block w-full py-2 font-bold text-center flex items-center">
-        <SingleLoader v-if="loadingMore"> </SingleLoader>
+      >
+        <SingleLoader v-if="loadingMore">
+        </SingleLoader>
         <span v-else>Load More</span>
       </button>
     </template>
@@ -38,6 +40,18 @@ export default {
       loadingMore: false
     }
   },
+  async fetch () {
+    await this.fetchNotifications({
+      page: this.page,
+      status: 'UNREAD',
+      limit: this.limit
+    })
+  },
+  computed: {
+    ...mapGetters({
+      unreadNotifications: 'notifications/getUnreadNotifications'
+    })
+  },
   methods: {
     ...mapActions({
       fetchNotifications: 'notifications/fetchNotifications'
@@ -52,18 +66,6 @@ export default {
       })
       this.loadingMore = false
     }
-  },
-  async fetch () {
-    await this.fetchNotifications({
-      page: this.page,
-      status: 'UNREAD',
-      limit: this.limit
-    })
-  },
-  computed: {
-    ...mapGetters({
-      unreadNotifications: 'notifications/getUnreadNotifications'
-    })
   }
 }
 </script>
