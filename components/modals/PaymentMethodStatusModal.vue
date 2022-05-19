8e0b17395f445afd23dde2ee5dec68b0b25a7a6d
Dapo Michaels <dapomichaels@gmail.com>
2022-05-19 16:54:43 +0100

    fix responsiveness of modal

--- components/modals/PaymentMethodStatusModal.vue
+++ components/modals/PaymentMethodStatusModal.vue
@@ -1,74 +1,84 @@
 <template>
-  <modal name="payment-method-status" height="auto" width="500" :adpative="true" :click-to-close="false">
-    <div class="p-6">
-      <template v-if="currentDisplay === 1">
-        <p class="text-3xl text-gray-700 font-normal pb-4">
-          Connect your Stripe or bank details
-        </p>
-        <p class="text-base font-normal text-gray-700">
-          We're making it easy to create, send and manage your finances. In order to create your first invoice, we just need to connect you up with the different providers!
-        </p>
-        <div class="mt-8 flex">
-          <button class="button-fill" @click.prevent="nextStep">
-            Proceed
-          </button>
-          <button class="bg-white-500 py-2 px-4 text-blue-500" @click="tryLater()">
-            Try it later
-          </button>
-        </div>
-      </template>
-      <template v-if="currentDisplay === 2">
-        <div class="flex justify-between items-center mb-5">
-          <p class="text-2xl font-normal text-gray-700">
-            Setup payment method
+  <modal 
+    name="payment-method-status"
+    class="p-0"
+    :width="532"
+    :click-to-close="false"
+    height="auto"
+    :adaptive="true"
+  >
+    <div class="grid m-6">
+      <div class="p-1">
+        <template v-if="currentDisplay === 1">
+          <p class="text-3xl text-gray-700 font-normal pb-4">
+            Connect your Stripe or bank details
           </p>
-        </div>
-        <div>
-          <ul>
-            <li class="flex justify-between items-center mb-8">
-              <div>
-                <p class="text-lg font-medium text-gray-700 mb-1">
-                  Connect to
-                  <span>
-                    <img
-                      class="h-6 inline-block"
-                      src="~/assets/img/stripe.png"
-                      alt="stripe logo"
-                    />
-                  </span>
-                </p>
-                <p class="text-sm font-normal text-gray-500">
-                  Connect your stripe account if you have one, or provide we'll help you create one.
-                </p>
-              </div>
-              <div class="flex">
-                <ButtonSpinner :loading="isStripeLoading" class="button-outline" @click="connectToStripeAndRedirect">
-                  Connect
-                </ButtonSpinner>
-              </div>
-            </li>
-            <li class="flex justify-between items-center mb-8">
-              <div>
-                <p class="text-lg font-medium text-gray-700 mb-1">
-                  Add deposit bank account
-                </p>
-                <p class="text-sm font-normal text-gray-500">
-                  We will never share your bank account details with anyone.
-                </p>
-              </div>
-              <div>
-                <button class="button-outline flex items-center" @click.prevent="$modal.show('payment-method-bank-account')">
-                  <i
-                    role="button"
-                    class="fi-rr-bank mr-2 text-blue-500 h-5 w-5"
-                  ></i>
-                  <span class="text-base font-medium">Setup</span>
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </template>
+          <p class="text-base font-normal text-gray-700">
+            We're making it easy to create, send and manage your finances.
+            In order to create your first invoice, we just need to connect you up with the different providers!
+          </p>
+          <div class="mt-8 flex">
+            <button class="button-fill" @click.prevent="nextStep">
+              Proceed
+            </button>
+            <button class="bg-white-500 py-2 px-4 text-blue-500" @click="tryLater()">
+              Try it later
+            </button>
+          </div>
+        </template>
+        <template v-if="currentDisplay === 2">
+          <div class="flex justify-between items-center mb-5">
+            <p class="text-2xl font-normal text-gray-700">
+              Setup payment method
+            </p>
+          </div>
+          <div>
+            <ul>
+              <li class="flex justify-between items-center mb-8">
+                <div>
+                  <p class="text-lg font-medium text-gray-700 mb-1">
+                    Connect to
+                    <span>
+                      <img
+                        class="h-6 inline-block"
+                        src="~/assets/img/stripe.png"
+                        alt="stripe logo"
+                      />
+                    </span>
+                  </p>
+                  <p class="text-sm font-normal text-gray-500">
+                    Connect your stripe account if you have one, or provide we'll help you create one.
+                  </p>
+                </div>
+                <div class="flex">
+                  <ButtonSpinner :loading="isStripeLoading" class="button-outline" @click="connectToStripeAndRedirect">
+                    Connect
+                  </ButtonSpinner>
+                </div>
+              </li>
+              <li class="flex justify-between items-center mb-8">
+                <div>
+                  <p class="text-lg font-medium text-gray-700 mb-1">
+                    Add deposit bank account
+                  </p>
+                  <p class="text-sm font-normal text-gray-500">
+                    We will never share your bank account details with anyone.
+                  </p>
+                </div>
+                <div>
+                  <button class="button-outline flex items-center" @click.prevent="$modal.show('payment-method-bank-account')">
+                    <i
+                      role="button"
+                      class="fi-rr-bank mr-2 text-blue-500 h-5 w-5"
+                    ></i>
+                    <span class="text-base font-medium">Setup</span>
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </template>
+      </div>
     </div>
   </modal>
 </template>
