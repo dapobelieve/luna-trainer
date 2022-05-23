88980670c06389233ca405ef080c8b5c95c2a1e6
robel_ephraim <ephraimrobel15@gmail.com>
2022-05-23 10:05:30 +0300

    label and title changes on scheduling section

--- components/modals/ScheduleWelcomeModal.vue
+++ components/modals/ScheduleWelcomeModal.vue
@@ -5,10 +5,12 @@
       <div class="grid m-6">
         <div class="py-0 text-justify">
           <div class="text-left mb-5 font-light text-2xl">
-            <h3>Welcome to scheduling</h3>
+            <h3>Welcome to scheduling.</h3>
           </div>
           <p class="mb-8 w-50 text-justify">
-            This is where you can manage all your session bookings. Take our short tour of the key features or explore yourself.
+            So, you've made it this far! 
+			To quickly get up to speed on the scheduling functions and features, you can take the tour. 
+			you can explore yourself.
           </p>
           <div class="flex justify-left gap-5">
             <button class="bg-blue-500 py-2 px-4 text-white" style="width:fit-content" @click="takeTour">
